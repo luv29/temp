a11bf7dc945c870ef962ca95a544dc784f13f0e0
luv29 <luvkansal29@gmail.com>
2024-12-06 22:04:39 +0530

    utils and configuration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 
 import dotenv from "dotenv";
 import connectDB from './db/index.js';
+import { app } from "./app.js";
+
+const port = process.env.PORT
 
 dotenv.config({
     path: './env'
 })
 
-connectDB()
+connectDB().then(()=> {
+    app.listen(port, ()=>{
+        console.log(`Process is listening on port ${port}`)
+    })
+}).catch((erro)=> {
+    console.log("MONGODB connection error: ", error);
+    process.exit(1);
+})
 
 /* if all in single file
 import mongoose from 'mongoose';
